8ae944129440e74ee8356fe515c23dda137fc150
aikchun <aikchun616@gmail.com>
2017-12-08 17:21:42 +0800

    Add two helper function to flatten and transform the flatten list to add an index as a tuple.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -36,6 +36,8 @@ defmodule Identicon do
     hex
     |> Enum.chunk(3)
     |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
